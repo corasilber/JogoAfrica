9a75dd6d4c6b458c4231dda20244aed694f44812
Augusto <augusto2112@me.com>
2017-12-02 16:55:53 -0200

    Commit timer

---
+++ app/src/main/java/com/lddm/jogoafrica/TimerActivity.java
@@ -0,0 +1,90 @@
+package com.lddm.jogoafrica;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by augusto on 02/12/17.
+ */
+
+public class TimerActivity extends AppCompatActivity {
+    public static String PALAVRAS = "WORDS";
+    public static String NOME_EQUIPE = "EQUIPE";
+    public static String NOME_JOGADOR = "JOGADOR";
+
+    private ArrayList<String> palavras;
+    private TextView nomeJogador;
+    private TextView nomeEquipe;
+    private TextView palavraTextView;
+    private TextView countDownTextView;
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        countDownTextView = (TextView) findViewById(R.id.countDownText);
+        nomeJogador = (TextView) findViewById(R.id.countDownText);
+        nomeEquipe = (TextView) findViewById(R.id.countDownText);
+        palavraTextView = (TextView) findViewById(R.id.countDownText);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            palavras = intent.getStringArrayListExtra(PALAVRAS);
+            nomeJogador.setText(intent.getStringExtra(NOME_JOGADOR));
+            nomeEquipe.setText(intent.getStringExtra(NOME_EQUIPE));
+        } else {
+            palavras = new ArrayList<String>();
+            palavras.add("Bolacha");
+            palavras.add("Figo");
+            palavras.add("Moita");
+            nomeJogador.setText("Augusto");
+            nomeEquipe.setText("Afrimaster");
+        }
+
+        new CountDownTimer(60000, 1000) {
+            @Override
+            public void onTick(long l) {
+                countDownTextView.setText(Long.toString(l / 1000));
+            }
+
+            @Override
+            public void onFinish() {
+                countDownTextView.setText("done!");
+            }
+        };
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
