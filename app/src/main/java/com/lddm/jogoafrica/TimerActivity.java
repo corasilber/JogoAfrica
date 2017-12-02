adf2b4054a96ea60bc4252b3ea3425b35ed4923e
Augusto <augusto2112@me.com>
2017-12-02 17:29:14 -0200

    Corrigido bug

--- app/src/main/java/com/lddm/jogoafrica/TimerActivity.java
+++ app/src/main/java/com/lddm/jogoafrica/TimerActivity.java
@@ -29,25 +29,27 @@ public class TimerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.timer_activity);
         countDownTextView = (TextView) findViewById(R.id.countDownText);
         nomeJogador = (TextView) findViewById(R.id.countDownText);
         nomeEquipe = (TextView) findViewById(R.id.countDownText);
         palavraTextView = (TextView) findViewById(R.id.countDownText);
 
-        Intent intent = getIntent();
-        if (intent != null) {
-            palavras = intent.getStringArrayListExtra(PALAVRAS);
-            nomeJogador.setText(intent.getStringExtra(NOME_JOGADOR));
-            nomeEquipe.setText(intent.getStringExtra(NOME_EQUIPE));
-        } else {
+//        Intent intent = getIntent();
+//        if (intent != null) {
+//            palavras = intent.getStringArrayListExtra(PALAVRAS);
+//            nomeJogador.setText(intent.getStringExtra(NOME_JOGADOR));
+//            nomeEquipe.setText(intent.getStringExtra(NOME_EQUIPE));
+//        } else {
             palavras = new ArrayList<String>();
             palavras.add("Bolacha");
             palavras.add("Figo");
             palavras.add("Moita");
             nomeJogador.setText("Augusto");
             nomeEquipe.setText("Afrimaster");
-        }
+//        }
 
+        palavraTextView.setText("60");
         new CountDownTimer(60000, 1000) {
             @Override
             public void onTick(long l) {
@@ -58,7 +60,7 @@ public class TimerActivity extends AppCompatActivity {
             public void onFinish() {
                 countDownTextView.setText("done!");
             }
-        };
+        }.start();
 
     }
 }
